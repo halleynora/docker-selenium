122241d7d77f7930c1adecda8b2abaf95c8bad5b
Halley Short <halley@studyblue.com>
2016-01-14 13:20:44 -0800

    no message

--- start-hub.sh
+++ start-hub.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -v ${HOME}/docker-selenium:/node/config -p 8080:4444 --name hub sb/grid_hub
+sudo docker run -d -v ${HOME}/docker-selenium:/node/config -p 8080:4444 --name hub erix/grid_hub
